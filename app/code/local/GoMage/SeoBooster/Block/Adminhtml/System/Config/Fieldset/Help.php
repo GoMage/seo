1e1268f5cb7935b638883289d12afb1a666427e1
Andrey Zac <az@stepcart.com>
2014-06-19 15:15:01 +0300

    gomage-6737

--- app/code/local/GoMage/SeoBooster/Block/Adminhtml/System/Config/Fieldset/Help.php
+++ app/code/local/GoMage/SeoBooster/Block/Adminhtml/System/Config/Fieldset/Help.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * GoMage Seo Booster Extension
  *
@@ -10,61 +11,62 @@
  * @version      Release: 1.0.0
  * @since        Available since Release 1.0.0
  */
-
 class GoMage_SeoBooster_Block_Adminhtml_System_Config_Fieldset_Help
     extends Mage_Adminhtml_Block_System_Config_Form_Fieldset
 {
-    /**
-     * Return header html for fieldset
-     *
-     * @param Varien_Data_Form_Element_Abstract $element
-     * @return string
-     */
     protected function _getHeaderHtml($element)
     {
-        if (method_exists(get_parent_class($this), '_getHeaderTitleHtml')) {
-            return parent::_getHeaderHtml($element);
+        if (method_exists($this, '_getHeaderTitleHtml')) {
+            if ($element->getIsNested()) {
+                $html = '<tr class="nested"><td colspan="4"><div class="' . $this->_getFrontendClass($element) . '">';
+            } else {
+                $html = '<div class="' . $this->_getFrontendClass($element) . '">';
+            }
+            $html .= $this->_getHeaderTitleHtml($element);
+
+            $html .= '<input id="' . $element->getHtmlId() . '-state" name="config_state[' . $element->getId()
+                . ']" type="hidden" value="' . (int)$this->_getCollapseState($element) . '" />';
+            $html .= '<fieldset class="' . $this->_getFieldsetCss($element) . '" id="' . $element->getHtmlId() . '">';
+            $html .= '<button style="margin-right:25px; margin-top:-33px;  float: right;" onclick="window.open(\'http://wiki.gomage.com/display/seobooster/Home\')" class="scalable go" type="button" id="gsb_help"><span>' . $this->__('GoMage SEO Booster Wiki') . '</span></button>';
+
+            $html .= '<legend>' . $element->getLegend() . '</legend>';
+
+            $html .= $this->_getHeaderCommentHtml($element);
+
+            // field label column
+            $html .= '<table cellspacing="0" class="form-list"><colgroup class="label" /><colgroup class="value" />';
+            if ($this->getRequest()->getParam('website') || $this->getRequest()->getParam('store')) {
+                $html .= '<colgroup class="use-default" />';
+            }
+            $html .= '<colgroup class="scope-label" /><colgroup class="" /><tbody>';
+        } else {
+            $default = !$this->getRequest()->getParam('website') && !$this->getRequest()->getParam('store');
+            $html    = '<div  class="entry-edit-head collapseable" >';
+            $html .= '<button style="margin-right:25px;float: right;" onclick="window.open(\'http://wiki.gomage.com/display/seobooster/Home\')" class="scalable go" type="button" id="gsb_help"><span>' . $this->__('GoMage SEO Booster Wiki') . '</span></button>';
+            $html .= '<a id="' . $element->getHtmlId() . '-head" href="#" onclick="Fieldset.toggleCollapse(\'' . $element->getHtmlId() . '\', \'' . $this->getUrl('*/*/state') . '\'); return false;">' . $element->getLegend() . '</a></div>';
+            $html .= '<input id="' . $element->getHtmlId() . '-state" name="config_state[' . $element->getId() . ']" type="hidden" value="' . (int)$this->_getCollapseState($element) . '" />';
+            $html .= '<fieldset class="' . $this->_getFieldsetCss() . '" id="' . $element->getHtmlId() . '">';
+            $html .= '<legend>' . $element->getLegend() . '</legend>';
+
+            if ($element->getComment()) {
+                $html .= '<div class="comment">' . $element->getComment() . '</div>';
+            }
+            // field label column
+            $html .= '<table cellspacing="0" class="form-list"><colgroup class="label" /><colgroup class="value" />';
+            if (!$default) {
+                $html .= '<colgroup class="use-default" />';
+            }
+            $html .= '<colgroup class="scope-label" /><colgroup class="" /><tbody>';
         }
-
-        $default = !$this->getRequest()->getParam('website') && !$this->getRequest()->getParam('store');
-
-        $html = '<div  class="entry-edit-head collapseable" >
-        <button style="margin-right:25px; float: right;" onclick="window.open(\''. $this->_getHelpUrl() .'\', \'_blank\')" class="scalable go" type="button" id="glc_help"><span>'.$this->__('GoMage SEO Booster Wiki').'</span></button>
-        <a id="'.$element->getHtmlId().'-head" href="#" onclick="Fieldset.toggleCollapse(\''.$element->getHtmlId().'\', \''.$this->getUrl('*/*/state').'\'); return false;">'.$element->getLegend().'</a></div>';
-        $html.= '<input id="'.$element->getHtmlId().'-state" name="config_state['.$element->getId().']" type="hidden" value="'.(int)$this->_getCollapseState($element).'" />';
-        $html.= '<fieldset class="'.$this->_getFieldsetCss().'" id="'.$element->getHtmlId().'">';
-        $html.= '<legend>'.$element->getLegend().'</legend>';
-
-        if ($element->getComment()) {
-            $html .= '<div class="comment">'.$element->getComment().'</div>';
-        }
-        // field label column
-        $html.= '<table cellspacing="0" class="form-list"><colgroup class="label" /><colgroup class="value" />';
-        if (!$default) {
-            $html.= '<colgroup class="use-default" />';
-        }
-        $html.= '<colgroup class="scope-label" /><colgroup class="" /><tbody>';
-
         return $html;
     }
 
-    /**
-     * Return header title part of html for fieldset
-     *
-     * @param Varien_Data_Form_Element_Abstract $element
-     * @return string
-     */
-    protected function _getHeaderTitleHtml($element)
+    protected function _getFieldsetCss($element = null)
     {
-        return '<div class="entry-edit-head collapseable" >
-        <button style="margin-right:25px; float: right;" onclick="window.open(\''. $this->_getHelpUrl() .'\', \'_blank\')" class="scalable go" type="button" id="glc_help"><span>'.$this->__('GoMage SEO Booster Wiki').'</span></button>
-        <a id="' . $element->getHtmlId()
-        . '-head" href="#" onclick="Fieldset.toggleCollapse(\'' . $element->getHtmlId() . '\', \''
-        . $this->getUrl('*/*/state') . '\'); return false;">' . $element->getLegend() . '</a></div>';
+        $configCss = (string)$this->getGroup($element)->fieldset_css;
+        return 'config collapseable' . ($configCss ? ' ' . $configCss : '');
     }
 
-    protected function _getHelpUrl()
-    {
-        return 'http://wiki.gomage.com/display/seobooster/Home';
-    }
+
 }
+
