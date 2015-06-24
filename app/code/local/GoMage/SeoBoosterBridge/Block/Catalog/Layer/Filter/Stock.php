fdce3ed397c90b106a3e7ec141d7872899951f12
Andrey Zac <az@stepcart.com>
2015-06-24 13:58:00 +0300

    copyrights

--- app/code/local/GoMage/SeoBoosterBridge/Block/Catalog/Layer/Filter/Stock.php
+++ app/code/local/GoMage/SeoBoosterBridge/Block/Catalog/Layer/Filter/Stock.php
@@ -4,11 +4,11 @@
  * GoMage Seo Booster Extension
  *
  * @category     Extension
- * @copyright    Copyright (c) 2013-2014 GoMage (http://www.gomage.com)
+ * @copyright    Copyright (c) 2013-2015 GoMage (http://www.gomage.com)
  * @author       GoMage
  * @license      http://www.gomage.com/license-agreement/  Single domain license
  * @terms of use http://www.gomage.com/terms-of-use/
- * @version      Release: 1.1.0
+ * @version      Release: 1.2.0
  * @since        Available since Release 1.2.0
  */
 class GoMage_SeoBoosterBridge_Block_Catalog_Layer_Filter_Stock extends GoMage_Navigation_Block_Catalog_Layer_Filter_Stock
